2c182f604a40213725ade8be9263d17e739e7b42
indrajith76 <indrajithgoswami@gmail.com>
2022-08-16 17:31:18 +0600

    final commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,52 @@
+/*---------------------------
+      code for dark mode 
+-----------------------------*/
+document.getElementById('bg-btn').addEventListener('click',function(){
+    // this code for bg-dark-mode button and change bg-color
+    document.getElementById('bg-btn').style.color='#4167a0';
+    document.body.style.backgroundColor = '#010d1e';
+
+    // this code for change list background for dark mode
+    const listItem = document.getElementsByClassName('list-group-item');
+    for (const li of listItem) {
+        li.style.backgroundColor='#041a3b';
+        li.classList = 'list-group-item d-flex justify-content-between text-secondary';
+        li.style.color='white';
+    }
+
+    // code of dark mode for list button
+    const libtn = document.getElementsByClassName('li-btn');
+    for (const btn of libtn) {
+        btn.style.backgroundColor='#7b010d';
+        btn.style.borderColor='#7b010d';
+        btn.style.color='#d6d6d6';
+    }
+
+    // code of dark mode for input field
+    const inputList = document.getElementById('input-list');
+    inputList.style.backgroundColor = '#041a3b';
+    inputList.style.borderColor = '#545454';
+    // code of top h1
+    const topH1 = document.getElementById('top-h1');
+    topH1.classList = '';
+    topH1.style.color = '#0057a1';
+    // code of add list input button
+    const inputBtn = document.getElementById('input-btn');
+    inputBtn.style.background='#08306b';
+    inputBtn.style.borderColor = '#595b5c';
+    inputBtn.style.color = '#c5c5c5';
+})
+
+/*--------------------------------
+    code for remove list item 
+----------------------------------*/
 document.getElementById('list-ul').addEventListener('click',function(event){
-    event.target.parentNode.remove(event.target)
+    event.target.parentNode.remove(event.target);
 });
 
-
+/*--------------------------------
+    code for add new list 
+----------------------------------*/
 document.getElementById('input-btn').addEventListener('click',function(){
     const inputList = document.getElementById('input-list');
     const inputListValue = inputList.value;
@@ -16,8 +60,10 @@ document.getElementById('input-btn').addEventListener('click',function(){
     deleteBtn.innerText = 'Delete';
     deleteBtn.classList = 'li-btn btn btn-danger';
     li.appendChild(deleteBtn);
-
+    
     listUl.appendChild(li);
-
+    
     inputList.value = '';
 });
+
+
