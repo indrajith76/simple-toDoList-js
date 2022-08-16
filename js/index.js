4feb8223bdd156d04babe7f34e0332ba92aa1f55
indrajith76 <indrajithgoswami@gmail.com>
2022-08-16 16:14:02 +0600

    complete dynamic work of list

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-const liButton = document.getElementsByClassName('li-btn');
-for (const liBtn of liButton) {
-    liBtn.addEventListener('click',function(event){
-        liBtn.parentNode.remove(event.target);
-    })
-}
+document.getElementById('list-ul').addEventListener('click',function(event){
+    event.target.parentNode.remove(event.target)
+});
 
 
 document.getElementById('input-btn').addEventListener('click',function(){
-    const inputValue = document.getElementById('input-list').value;
+    const inputList = document.getElementById('input-list');
+    const inputListValue = inputList.value;
     
-    const listUl = document.getElementById('list-ul')
+    const listUl = document.getElementById('list-ul');
     const li = document.createElement('li');
-    li.innerText = inputValue;
+    li.innerText = inputListValue;
     li.classList = 'list-group-item d-flex justify-content-between text-primary';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.classList = 'li-btn btn btn-danger';
+    li.appendChild(deleteBtn);
+
     listUl.appendChild(li);
-})
+
+    inputList.value = '';
+});
